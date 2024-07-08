9dc0ff64654bf6bc5ab9dc3385fdc577bcdc2617
Ugbe <140442305+Ugbe@users.noreply.github.com>
2024-07-09 00:35:25 +0100

    Update urls.py

--- stagetwo/urls.py
+++ stagetwo/urls.py
@@ -10,11 +10,11 @@ from .views import (
 )
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('users/<uuid:user_id>/', UserDetailView.as_view(), name='user-detail'),
-    path('organisations/', OrganisationListView.as_view(), name='organisation-list'),
-    path('organisations/<uuid:org_id>/', OrganisationDetailView.as_view(), name='organisation-detail'),
-    path('organisations/', OrganisationCreateView.as_view(), name='organisation-create'),
-    path('organisations/<uuid:org_id>/users/', AddUserToOrganisationView.as_view(), name='add-user-to-organisation'),
+    path('register', UserRegistrationView.as_view(), name='register'),
+    path('login', UserLoginView.as_view(), name='login'),
+    path('users/<uuid:user_id>', UserDetailView.as_view(), name='user-detail'),
+    path('organisations', OrganisationListView.as_view(), name='organisation-list'),
+    path('organisations/<uuid:org_id>', OrganisationDetailView.as_view(), name='organisation-detail'),
+    path('organisations', OrganisationCreateView.as_view(), name='organisation-create'),
+    path('organisations/<uuid:org_id>/users', AddUserToOrganisationView.as_view(), name='add-user-to-organisation'),
 ]
