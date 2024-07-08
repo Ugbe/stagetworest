875e502bfa09c4f5dc7aca9ff7a9fc8e91a5f39b
Ugbe <140442305+Ugbe@users.noreply.github.com>
2024-07-08 23:26:33 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@
 
 #Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 echo "Migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput
+python3.12 manage.py collectstatic --noinput
 >>>>>>> c85b2298e5d0f5005fa87b3ee7b2b72d93b910d1
