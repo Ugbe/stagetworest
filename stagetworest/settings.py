0303bc0f5e3493fe37ca6e4e910f01593c65a1d7
Ugbe <ugbedeojoabba@gmail.com>
2024-07-09 08:39:37 +0100

    Edited .env and settings

--- stagetworest/settings.py
+++ stagetworest/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
 import os
 from dotenv import load_dotenv
 
@@ -92,9 +91,21 @@ WSGI_APPLICATION = "stagetworest.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+} """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
+    }
 }
+
 
 
 # Password validation
